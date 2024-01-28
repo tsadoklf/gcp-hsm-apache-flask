90ecf73de8c3925349bd71750c22c996f22a3dca
Amir Netiv <amirnse@users.noreply.github.com>
2024-01-28 20:52:01 +0200

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -52,7 +52,8 @@ def whitelist(ip_whitelist, domain_whitelist):
         @wraps(func)
         def wrapper(*args, **kwargs):
             # client_ip = request.remote_addr
-            client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+            # client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+            client_ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
 
             if client_ip not in ip_whitelist:
                 try:
